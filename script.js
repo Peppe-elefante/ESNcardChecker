f501c21741fb12eee13a3948e62b4b944d3c4175
Giuseppe Elefante <giuseppeelefante123@gmail.com>
2025-05-17 12:58:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,149 @@
+var APIkey ;
+var OrgId ;
+const erasmus = [];
+const eventParticipants = [];
+
+fetch("/erasmus.json")
+  .then((res) => res.json())
+  .then((data) => {
+    erasmus.push(...data);
+  });
+
+const title = document.getElementById("title");
+const body = document.getElementById("text");
+const modalBody = document.getElementById("modalBody");
+const button = document.getElementById("button");
+
+fetchAllAttendees(title).then((attendees) => {
+  const ul = document.createElement("ul");
+  ul.className = "list-group"; // Bootstrap list group
+
+  for (const attendee of attendees) {
+    const name = attendee.profile.name ?? "Unknown";
+    const esnCard = attendee.answers?.[0]?.answer ?? "N/A";
+    eventParticipants.push({ fullName: name, ESNCard: esnCard });
+
+    const li = document.createElement("li");
+    li.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+    li.innerHTML = `
+      <span><strong>${name}</strong></span>
+      <span class="badge bg-primary rounded-pill">${esnCard}</span>
+    `;
+    ul.appendChild(li);
+  }
+
+  body.innerHTML = "";
+  body.appendChild(ul);
+  button.classList.remove("disabled");
+  button.addEventListener("click", compare);
+});
+
+const compare = () => {
+  modalBody.innerHTML = '<ul class="list-group">';
+  for (const participant of eventParticipants) {
+    let flag = true;
+    for (const eras of erasmus) {
+      if (eras.ESNCard === participant.ESNCard) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      modalBody.innerHTML += `<li class="list-group-item list-group-item-danger border border-secondary"> <b>NOME:</b> ${participant.fullName}      <b>ESNCARD:</b> ${participant.ESNCard} </li>`;
+    }
+  }
+  modalBody.innerHTML += "</ul>";
+};
+
+async function getNpages() {
+  try {
+    const response = await fetch(
+      `https://www.eventbriteapi.com/v3/organizations/${OrgId}/events/`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${APIkey}`,
+        },
+      }
+    );
+    const data = await response.json();
+    const pages = data.pagination.page_count;
+    return pages;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
+
+async function getId(title) {
+  try {
+    const totalPages = await getNpages();
+    const response = await fetch(
+      `https://www.eventbriteapi.com/v3/organizations/${OrgId}/events/?page=${totalPages}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${APIkey}`,
+        },
+      }
+    );
+    const data = await response.json();
+    const lastEvent = data.events[data.events.length - 1];
+    title.textContent = lastEvent.name.text;
+    return lastEvent.id;
+  } catch (error) {
+    console.error("Error fetching last event ID:", error);
+    return null;
+  }
+}
+
+async function fetchAllAttendees(title) {
+  await fetchEnv()
+  const baseUrl = `https://www.eventbriteapi.com/v3/events/`;
+  const headers = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${APIkey}`,
+    },
+  };
+  const allAttendees = [];
+
+  try {
+    const eventId = await getId(title);
+    const firstResponse = await fetch(
+      `${baseUrl}${eventId}/attendees/`,
+      headers
+    );
+    const firstData = await firstResponse.json();
+
+    const totalPages = firstData.pagination.page_count;
+    allAttendees.push(...firstData.attendees);
+
+    for (let page = 2; page <= totalPages; page++) {
+      const response = await fetch(
+        `${baseUrl}${eventId}/attendees/?page=${page}`,
+        headers
+      );
+      const data = await response.json();
+      allAttendees.push(...data.attendees);
+    }
+
+    return allAttendees;
+  } catch (error) {
+    console.error("Error fetching pages:", error);
+    return [];
+  }
+}
+
+
+async function fetchEnv() {
+  try {
+    const res = await fetch("/env.json");
+    const data = await res.json();
+    APIkey = data.APIKey
+    OrgId = data.OrgId
+  } catch (error) {
+    console.error("Error fetching env.json:", error);
+  }
+}
