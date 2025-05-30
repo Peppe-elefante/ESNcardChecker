94829ef920affa683ec74279f2433ba6ff0f8e16
Giuseppe Elefante <giuseppeelefante123@gmail.com>
2025-05-30 15:54:49 +0200

    Added option to view previous events

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var APIkey ;
-var OrgId ;
+var APIkey;
+var OrgId;
 const erasmus = [];
 const eventParticipants = [];
 
@@ -13,31 +13,52 @@ const title = document.getElementById("title");
 const body = document.getElementById("text");
 const modalBody = document.getElementById("modalBody");
 const button = document.getElementById("button");
+const eventBtn = document.getElementById("eventBtn");
+var eventSelector = 1;
 
-fetchAllAttendees(title).then((attendees) => {
-  const ul = document.createElement("ul");
-  ul.className = "list-group"; // Bootstrap list group
+window.onload = function() {
+    eventHandler(1);
+  };
 
-  for (const attendee of attendees) {
-    const name = attendee.profile.name ?? "Unknown";
-    const esnCard = attendee.answers?.[0]?.answer ?? "N/A";
-    eventParticipants.push({ fullName: name, ESNCard: esnCard });
+function eventoPrecedente() {
+  eventSelector++;
+  console.log("chiamato evento");
+  eventHandler(eventSelector);
+  title.innerHTML = "Loading"
+  modalBody.innerHTML = "Loading"
+  document.querySelectorAll('.list-group').forEach(function(el) {
+  el.innerHTML = 'Loading';
+});
+}
 
-    const li = document.createElement("li");
-    li.className =
-      "list-group-item d-flex justify-content-between align-items-center";
-    li.innerHTML = `
+eventBtn.addEventListener("click", eventoPrecedente);
+
+function eventHandler(eventSelector = 1) {
+  fetchAllAttendees(title, eventSelector).then((attendees) => {
+    const ul = document.createElement("ul");
+    ul.className = "list-group"; // Bootstrap list group
+
+    for (const attendee of attendees) {
+      const name = attendee.profile.name ?? "Unknown";
+      const esnCard = attendee.answers?.[0]?.answer ?? "N/A";
+      eventParticipants.push({ fullName: name, ESNCard: esnCard });
+
+      const li = document.createElement("li");
+      li.className =
+        "list-group-item d-flex justify-content-between align-items-center";
+      li.innerHTML = `
       <span><strong>${name}</strong></span>
       <span class="badge bg-primary rounded-pill">${esnCard}</span>
     `;
-    ul.appendChild(li);
-  }
+      ul.appendChild(li);
+    }
 
-  body.innerHTML = "";
-  body.appendChild(ul);
-  button.classList.remove("disabled");
-  button.addEventListener("click", compare);
-});
+    body.innerHTML = "";
+    body.appendChild(ul);
+    button.classList.remove("disabled");
+    button.addEventListener("click", compare);
+  });
+}
 
 const compare = () => {
   modalBody.innerHTML = '<ul class="list-group">';
@@ -76,7 +97,7 @@ async function getNpages() {
   }
 }
 
-async function getId(title) {
+async function getId(title, eventSelector = 1) {
   try {
     const totalPages = await getNpages();
     const response = await fetch(
@@ -89,7 +110,8 @@ async function getId(title) {
       }
     );
     const data = await response.json();
-    const lastEvent = data.events[data.events.length - 1];
+    //TO FIX
+    const lastEvent = data.events[data.events.length - 1 * eventSelector];
     title.textContent = lastEvent.name.text;
     return lastEvent.id;
   } catch (error) {
@@ -98,8 +120,8 @@ async function getId(title) {
   }
 }
 
-async function fetchAllAttendees(title) {
-  await fetchEnv()
+async function fetchAllAttendees(title, eventSelector = 1) {
+  await fetchEnv();
   const baseUrl = `https://www.eventbriteapi.com/v3/events/`;
   const headers = {
     method: "GET",
@@ -110,7 +132,7 @@ async function fetchAllAttendees(title) {
   const allAttendees = [];
 
   try {
-    const eventId = await getId(title);
+    const eventId = await getId(title, eventSelector);
     const firstResponse = await fetch(
       `${baseUrl}${eventId}/attendees/`,
       headers
@@ -136,13 +158,12 @@ async function fetchAllAttendees(title) {
   }
 }
 
-
 async function fetchEnv() {
   try {
     const res = await fetch("/env.json");
     const data = await res.json();
-    APIkey = data.APIKey
-    OrgId = data.OrgId
+    APIkey = data.APIKey;
+    OrgId = data.OrgId;
   } catch (error) {
     console.error("Error fetching env.json:", error);
   }
